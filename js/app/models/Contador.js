384f4327e6e3be31a4ada77965c5fc64707a4220
Vinícius X <viniciusxavier2212@gmail.com>
2020-03-19 22:01:12 -0300

    Pequenas refatorações e ajustes em layout responsivo.

--- js/app/models/Contador.js
+++ js/app/models/Contador.js
@@ -1,79 +1,90 @@
 class Contador {
+  constructor(text) {
+    this._text = text;
+  }
 
-    constructor(text) {
-        this._text = text;
-    }
+  set texto(texto) {
+    this._text = texto;
+  }
 
-    set texto(texto) {
-        this._text = texto;
-    }
+  getNumCaracteres() {
+    // retorna o número de caracteres
+    return this._text.length;
+  }
 
-    getNumCaracteres() { // retorna o número de caracteres
-        return this._text.length;
-    }
+  getNumLetras() {
+    // retorna o número de letras
+    return this.getLetras().length;
+  }
 
-    getNumLetras() { // retorna o número de letras
-        return this.getLetras().length;
-    }
+  getCaracteres() {
+    // retorna os caracteres como um array
+    return this._text.split("");
+  }
 
-    getCaracteres() { // retorna os caracteres como um array
-        return this._text.split('');
-    }
+  getLetras() {
+    // retorna uma string com apenas as letras
+    return this._text
+      .split("")
+      .filter(
+        letra =>
+          (letra.charCodeAt(0) >= 65 && letra.charCodeAt(0) <= 90) ||
+          (letra.charCodeAt(0) >= 97 && letra.charCodeAt(0) <= 122) ||
+          (letra.charCodeAt(0) >= 128 && letra.charCodeAt(0) <= 165)
+      )
+      .join("")
+      .toLowerCase();
+  }
 
-    getLetras() { // retorna uma string com apenas as letras
-        return this._text
-            .split('')
-            .filter(letra => 
-                letra.charCodeAt(0) >= 65 && letra.charCodeAt(0) <= 90 ||
-                letra.charCodeAt(0) >= 97 && letra.charCodeAt(0) <= 122 ||
-                letra.charCodeAt(0) >= 128 && letra.charCodeAt(0) <= 165
-            )
-            .join('')
-            .toLowerCase();
-    }
+  getNumCaractere(char) {
+    // retorna o número 'char' no texto
+    return this._text.split("").filter(c => c == char).length;
+  }
 
-    getNumCaractere(char) { // retorna o número 'char' no texto 
-        return this._text
-            .split('')
-            .filter(c => c == char ? true : false)
-            .length;
-    } 
+  getNumLetra(letra) {
+    // retorna o número 'letra' no texto
+    return this._text
+      .replace(/\s/g, "")
+      .split("")
+      .filter(l => l.toLowerCase() == letra.toLowerCase()).length;
+  }
 
-    getNumLetra(letra) { // retorna o número 'letra' no texto
-        return this._text
-            .replace(/\s/g, '')
-            .split('')
-            .filter(l => l.toLowerCase() == letra.toLowerCase() ? true : false)
-            .length;
-    }
+  getNumLetrasAll() {
+    // retorna um objeto contendo a quantidade de cada letra
+    return this.getLetras()
+      .split("")
+      .sort((l1, l2) => l1.charCodeAt(0) - l2.charCodeAt(0))
+      .reduce((letras, letra) => {
+        if (!letras.hasOwnProperty(letra.toUpperCase()))
+          letras[letra.toUpperCase()] = 0;
+        letras[letra.toUpperCase()]++;
+        return letras;
+      }, {});
+  }
 
-    getNumLetrasAll() { // retorna um objeto contendo a quantidade de cada letra 
-        return this.getLetras()
-            .split('')
-            .sort((l1, l2) => l1.charCodeAt(0) - l2.charCodeAt(0))
-            .reduce((letras, letra) => {
-                if(!letras.hasOwnProperty(letra.toUpperCase())) letras[letra.toUpperCase()] = 0;
-                letras[letra.toUpperCase()]++;
-                return letras;
-            }, {});
-    }
+  getPorcentagemCaractere(char) {
+    // retorna a porcentagem de 'char' no texto
+    return parseFloat(
+      ((this.getNumCaractere(char) / this.getNumCaracteres()) * 100).toFixed(2)
+    );
+  }
 
-    getPorcentagemCaractere(char) { // retorna a porcentagem de 'char' no texto
-        return parseFloat((this.getNumCaractere(char) / this.getNumCaracteres() * 100).toFixed(2));
-    }
+  getPorcentagemLetra(letra) {
+    // retorna a porcentagem de 'letra' no texto (em relação ao total de letras)
+    return parseFloat(
+      ((this.getNumLetra(letra) / this.getNumLetras()) * 100).toFixed(2)
+    );
+  }
 
-    getPorcentagemLetra(letra) { // retorna a porcentagem de 'letra' no texto (em relação ao total de letras)
-        return parseFloat((this.getNumLetra(letra) / this.getNumLetras() * 100).toFixed(2));
-    }
-
-    getPorcentagemLetras() { // retorna um objeto contendo a porcentagem de cada letra no texto (em relação ao total de letras)
-        return this.getLetras()
-            .split('')
-            .reduce((letras, letra) => {
-                if(!letras.hasOwnProperty(letra.toUpperCase())) {
-                    letras[letra.toUpperCase()] = this.getPorcentagemLetra(letra);
-                }
-                return letras;
-            }, {});
-    }
+  getPorcentagemLetras() {
+    // retorna um objeto contendo a porcentagem de cada letra no texto (em relação ao total de letras)
+    return this.getLetras()
+      .split("")
+      .reduce((letras, letra) => {
+        if (!letras.hasOwnProperty(letra.toUpperCase())) {
+          letras[letra.toUpperCase()] = this.getPorcentagemLetra(letra);
+        }
+        return letras;
+      }, {});
+  }
 }
--- js/app/views/LettersView.js
+++ js/app/views/LettersView.js
@@ -1,37 +1,47 @@
 class LettersView extends View {
+  constructor(elemento) {
+    super(elemento);
+  }
 
-    constructor(elemento) {
-
-        super(elemento);
-    }
-
-    template(model) {
-        return `
+  template(model) {
+    return `
             <thead>
                 <tr>
-                    <th class="has-text-centered" colspan="${model.letters.getOrElse([null]).length + 1}">Letras</th>
+                    <th class="has-text-centered letra-th" colspan="${model.letters.getOrElse(
+                      [null]
+                    ).length + 1}">Letras</th>
                 </tr>
                 <tr>
                     <th></th>
-                    ${model.letters.getOrElse(['-']).map(letter =>
-                        `<th class="has-text-centered">${letter}</th>`
-                    ).join('')}
+                    ${model.letters
+                      .getOrElse(["-"])
+                      .map(
+                        letter => `<th class="has-text-centered">${letter}</th>`
+                      )
+                      .join("")}
                 </tr>
             </thead>
             <tbody>
                 <tr>
                     <th class="has-text-centered" title="Quantidade">N°</th>
-                    ${model.amount.getOrElse(['-']).map(amt =>
-                        `<td class="has-text-centered">${amt}</td>`
-                    ).join('')}
+                    ${model.amount
+                      .getOrElse(["-"])
+                      .map(amt => `<td class="has-text-centered">${amt}</td>`)
+                      .join("")}
                 </tr>
                 <tr>
                     <th class="has-text-centered" title="Frequência">%</th>
-                    ${model.frequence.getOrElse([0.0]).map(freq =>
-                        `<td class="has-text-centered" title="${freq}">${freq.toFixed(1)}</td>`
-                    ).join('')}
+                    ${model.frequence
+                      .getOrElse([0.0])
+                      .map(
+                        freq =>
+                          `<td class="has-text-centered" title="${freq}">${freq.toFixed(
+                            1
+                          )}</td>`
+                      )
+                      .join("")}
                 </tr>
             </tbody>
         `;
-    }
+  }
 }
